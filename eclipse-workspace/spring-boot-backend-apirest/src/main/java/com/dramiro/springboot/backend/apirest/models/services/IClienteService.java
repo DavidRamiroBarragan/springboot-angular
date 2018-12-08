4e35ef02f517faacfc125698ad6c2978542c728e
Dramiro <dramirobarragan@gmail.com>
2018-12-08 12:19:54 +0100

    Creación del Servicio IClienteService
    Creamos lo necesario para poner en funcionamiento un CRUD para la entity Cliente

---
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/dramiro/springboot/backend/apirest/models/services/IClienteService.java
@@ -0,0 +1,9 @@
+package com.dramiro.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import com.dramiro.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+	public List<Cliente>findAll();
+}
---
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/dramiro/springboot/backend/apirest/models/services/IClienteServiceImpl.java
@@ -0,0 +1,22 @@
+package com.dramiro.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dramiro.springboot.backend.apirest.models.dao.IClienteDao;
+import com.dramiro.springboot.backend.apirest.models.entity.Cliente;
+
+public class IClienteServiceImpl implements IClienteService {
+	
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@Override
+	@Transactional(readOnly=true)
+	public List<Cliente> findAll() {
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
+}
