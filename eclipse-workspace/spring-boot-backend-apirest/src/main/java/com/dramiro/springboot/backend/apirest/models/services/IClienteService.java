76f485975e96dcd4e3250a4a4994aa9f98ffb64b
Dramiro <dramirobarragan@gmail.com>
2018-12-15 18:00:28 +0100

    Comenzando Crud

--- eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/dramiro/springboot/backend/apirest/models/services/IClienteService.java
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/dramiro/springboot/backend/apirest/models/services/IClienteService.java
@@ -6,4 +6,7 @@ import com.dramiro.springboot.backend.apirest.models.entity.Cliente;
 
 public interface IClienteService {
 	public List<Cliente>findAll();
+	public Cliente save(Cliente cliente);
+	public void delete (Long id);
+	public Cliente findById(Long id);
 }
